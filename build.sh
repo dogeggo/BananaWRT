ed9e201245391e38f299cc9bfe498b3a20a1a189
dogeggo <32089821+dogeggo@users.noreply.github.com>
2025-04-22 03:21:48 +0800

    5G-Modem-Support

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ git clone https://github.com/immortalwrt/immortalwrt -b v24.10.1 immortalwrt
 cp -r dts/nightly/* immortalwrt/target/linux/mediatek/dts/
 cp -r 5G-Modem/5G-Modem-Support immortalwrt/package
 cp .config immortalwrt/
-cd immortalwrt
+cd immortalwrt/package
+wget -r -np -nH --cut-dirs=3 -R "index.html*" https://repo.superkali.me/releases/24.10.0/packages/additional_pack/
+cd ..
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 make defconfig
