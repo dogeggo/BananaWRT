b569960a80bf98beda94b751486eeb3b05d57fee
SuperKali <hello@superkali.me>
2025-05-27 19:48:48 +0000

    workflow: testing metadata generator into the bananawrt system

---
+++ scripts/utils/metadata-generator.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+#
+# File name: metadata-generator.sh
+# Description: Script to inject BananaWRT metadata into the build
+#
+# Copyright (c) 2024-2025 SuperKali <hello@superkali.me>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Check if RELEASE_TAG is set
+if [ -z "$RELEASE_TAG" ]; then
+    echo "Error: RELEASE_TAG is not set!"
+    exit 1
+fi
+
+# Get other variables from environment
+BUILD_DATE="$RELEASE_TAG"
+GITHUB_SHA="${GITHUB_SHA:-unknown}"
+SHORT_SHA="${GITHUB_SHA:0:7}"
+GITHUB_REF="${GITHUB_REF:-unknown}"
+BRANCH="${GITHUB_REF##*/}"
+RELEASE_TYPE="${BANANAWRT_RELEASE:-stable}"
+
+# Create the BananaWRT release file
+cat > package/base-files/files/etc/bananawrt_release << EOF
+# BananaWRT Release Information
+BANANAWRT_BUILD_DATE="${BUILD_DATE}"
+BANANAWRT_COMMIT="${GITHUB_SHA}"
+BANANAWRT_COMMIT_SHORT="${SHORT_SHA}"
+BANANAWRT_BRANCH="${BRANCH}"
+BANANAWRT_TYPE="${RELEASE_TYPE}"
+EOF
+
+echo "BananaWRT metadata injected successfully!"
+echo "Release Tag: ${RELEASE_TAG}"
+echo "Build Type: ${RELEASE_TYPE}"
+echo "Git Commit: ${SHORT_SHA}"
