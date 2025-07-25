20c1a84badad7995303474fdc5573e8e289d142e
Dong <32089821+dogeggo@users.noreply.github.com>
2025-07-25 10:02:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export FORCE_UNSAFE_CONFIGURE=1
-git clone https://github.com/immortalwrt/immortalwrt -b v24.10.1 immortalwrt
+git clone https://github.com/immortalwrt/immortalwrt -b v24.10.2 immortalwrt
 cp -r dts/nightly/* immortalwrt/target/linux/mediatek/dts/
 cp -r 5G-Modem/5G-Modem-Support immortalwrt/package
 cp .config immortalwrt/
 cd immortalwrt/package
-wget -r -np -nH --cut-dirs=3 -R "index.html*" https://repo.superkali.me/releases/24.10.0/packages/additional_pack/
+wget -r -np -nH --cut-dirs=3 -R "index.html*" https://repo.superkali.me/releases/24.10.2/packages/additional_pack/
 cd ..
 ./scripts/feeds update -a
 ./scripts/feeds install -a
